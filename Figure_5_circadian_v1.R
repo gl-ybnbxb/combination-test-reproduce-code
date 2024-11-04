934df345b767449c4d35b8b7332166ec752e71ec
Yuchao Jiang <yuchaojiang@users.noreply.github.com>
2024-11-04 11:52:16 -0600

    Add files via upload

---
+++ Figure_5_circadian_v1.R
@@ -0,0 +1,61 @@
+setwd("~/Library/CloudStorage/Dropbox/Collaborations/cauchy_combination/github_figures_5_6")
+library(MetaCycle)
+library(gap)
+library(rmutil)
+
+source('data_scripts/runJTK.R') # Original JTK method
+source('data_scripts/runJTK_combination.R') # Modified with combination test
+source('data_scripts/transformation_test_new.R')
+
+# Load the circadian gene expression data
+hughes=read.csv('data_scripts/hughes.csv')
+cycMouseLiverRNA = hughes
+rm(hughes)
+
+# Positive and negative controls
+load('data_scripts/gene.symbl.p.rda')
+load('data_scripts/gene.symbl.n.rda')
+length(gene.symbl.p); length(gene.symbl.n)
+gene.symbl.n=gene.symbl.n[gene.symbl.n%in%cycMouseLiverRNA$hughes.anno...4.] 
+gene.symbl.p=gene.symbl.p[gene.symbl.p%in%cycMouseLiverRNA$hughes.anno...4.]
+length(gene.symbl.p); length(gene.symbl.n)
+
+# Focus on the positive and negative control genes (comment out to run on genome wide)
+cycMouseLiverRNA=cycMouseLiverRNA[match(c(gene.symbl.p, gene.symbl.n), cycMouseLiverRNA$hughes.anno...4.),]
+
+JTK.results=as.data.frame(runJTK(cycMouseLiverRNA, JTKtime = 18:65))
+output=JTK.results[,c(1,3)]
+colnames(output)[2]='JTK'
+output[,2]=as.numeric(output[,2])
+
+for(test in c("Fisher", "Bonferroni", "Cauchy", "truncated_0.9",
+              "Pareto", "Frechet", "Levy")){
+  cat(test,'\n')
+  JTK.results=as.data.frame(runJTK_combination(cycMouseLiverRNA, JTKtime = 18:65, method=test))
+  output=cbind(output, as.numeric(JTK.results$ADJ.P))
+  colnames(output)[ncol(output)]=test
+  cat('\n')
+}
+head(output)
+
+
+
+output$JTK=as.numeric(output$JTK)
+plot(output$JTK, output$Bonferroni) # JTK implements Bonferroni
+
+test.output=output[,c('Bonferroni','Levy','Cauchy','truncated_0.9','Frechet','Pareto','Fisher')]
+rownames(test.output)=output$CycID
+
+c.col=c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+par(mfrow=c(1,2))
+par(mar=c(8.1, 4.1, 4.1, 2.1))
+boxplot(((test.output[match(gene.symbl.p, rownames(test.output)),])), 
+        ylab='p-value', main='Positive control genes',las=2, 
+        col=c.col, ylim=c(0,0.005), pch=16, cex=0.5)
+boxplot(((test.output[match(gene.symbl.n, rownames(test.output)),])), 
+        ylab='p-value', main='Negative control genes', las=2,
+        col=c.col, ylim=c(0,1), pch=16, cex=0.5)
+par(mfrow=c(1,1))
+par(mar=c(5.1, 4.1, 4.1, 2.1))
+
+
---
+++ Figure_6_GWAS_v3.R
@@ -0,0 +1,55 @@
+setwd("~/Library/CloudStorage/Dropbox/Collaborations/cauchy_combination/github_figures_5_6")
+
+library(pheatmap)
+library(rmutil)
+source('data_scripts/transformation_test_new.R')
+
+load('data_scripts/gwas.rda')
+
+output=matrix(nrow=nrow(G_list), ncol=10)
+for(i in 1:nrow(output)){
+  if(i%%100==0) cat(i,'\t')
+  gene=G_list[i,2]
+  gene.id=G_list[i,1]
+  # SNP-level p-values
+  pvals=gwas.calls[[gene.id]]$gwasP
+  pvals.output=c(N.snps=length(pvals),
+                 Epic=signif(epic.calls[i],3),  # EPIC p-value
+                 Magma=signif(magma.calls[i],3),  # MAGMA p-value
+                 Bonferroni=signif(transformation_global_test(pvals, method='Bonferroni'),3),
+                 Levy=signif(transformation_global_test(pvals, method='Levy'),3),
+                 Cauchy=signif(transformation_global_test(pvals, method='Cauchy'),3),
+                 truncated_0.9=signif(transformation_global_test(pvals, method='truncated_0.9'),3),
+                 Frechet=signif(transformation_global_test(pvals, method='Frechet'),3),
+                 Pareto=signif(transformation_global_test(pvals, method='Pareto'),3),
+                 Fisher=signif(transformation_global_test(pvals, method='Fisher'),3))
+  output[i,]=pvals.output
+  colnames(output)=names(pvals.output)
+}
+output=as.data.frame(output)
+output=cbind(gene.ensembl=G_list[,1],
+             gene=G_list[,2],
+             output)
+summary(output$N.snps)
+
+cbPalette <- c("gray30", "gray80", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# Call significant genes after FDR control
+# And compare the number of significant genes across methods
+for(fdr.thresh in c(0.05, 0.2)){
+  overlap.mat=matrix(nrow=ncol(output)-3, ncol=ncol(output)-3)
+  rownames(overlap.mat)=colnames(overlap.mat)=colnames(output)[-(1:3)]
+  for(i in 1:nrow(overlap.mat)){
+    for(j in i:ncol(overlap.mat)){
+      i.rej= which(p.adjust(output[,rownames(overlap.mat)[i]], method='fdr') < fdr.thresh)
+      j.rej= which(p.adjust(output[,colnames(overlap.mat)[j]], method='fdr') < fdr.thresh)
+      overlap.mat[i,j]=length(intersect(i.rej, j.rej))
+    }
+  }
+  overlap.mat.txt=(overlap.mat)
+  overlap.mat.txt[is.na(overlap.mat.txt)]=''
+  pdf(file=paste0('pval_sig_genes_SCZ_',fdr.thresh,'_v3.pdf'), width=6, height=6)
+  pheatmap(log(overlap.mat), cluster_rows=F, cluster_cols=F, na_col="white", display_numbers = overlap.mat.txt,
+           legend = FALSE, main=paste('No. of sig. genes after FDR control:',fdr.thresh,'SCZ'))
+  dev.off()
+}
